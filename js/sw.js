788a431619bf09e23f767daf1fd9b8251087f595
vannyhosal51017020 <vannyhosal@gmail.com>
2019-12-10 16:14:28 +0800

    second

--- js/sw.js
+++ js/sw.js
@@ -10,6 +10,7 @@ var urlsToCache = [
   "/img/bioskop720.jpg",
   "/img/bioskop480.jpg",
   "/css/style.css",
+  "/manifest.json,"
 ];
 self.addEventListener("install", function(event) {
   event.waitUntil(
@@ -17,3 +18,34 @@ self.addEventListener("install", function(event) {
 
     })
 })
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+    caches
+      .match(event.request, { cacheName: CACHE_NAME})
+      .then (function (response) {
+        if (response) {
+          console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+          return response;
+        }
+
+        console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
+        return fetch (event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map (function (cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
